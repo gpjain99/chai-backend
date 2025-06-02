fcfd008df1c1560b078f259b946c03de300845be
gpjain99 <gautam4288.gp@gmail.com>
2025-06-03 00:47:05 +0530

    new changes utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 
 
@@ -7,7 +8,17 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mogo db conn error failed",err);
+});
+
+
 /*
 import express from 'express';
 const app=express();
